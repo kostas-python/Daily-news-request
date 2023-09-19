7761a6727bc3736922a8ee662106ccab4ca2f745
kostas-python <diamantopoulos.kostas@hotmail.com>
2023-09-19 23:22:24 +0100

    Day 27: Access the news titles

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
-import json
+from send_email import send_email
 
 api_key = "b64e47ba730746dd988f986a5b38add8"
-url = "https://newsapi.org/v2/everything?q=tesla" \
-      "&from=2023-08-19&sortBy=publishedAt&apiKey=" \
-    "b64e47ba730746dd988f986a5b38add8"
+url = "https://newsapi.org/v2/everything?q=tesla&" \
+      "sortBy=publishedAt&apiKey=" \
+      "b64e47ba730746dd988f986a5b38add8"
 
 # Make request
 request = requests.get(url)
@@ -13,6 +13,12 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
+
+
